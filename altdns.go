9e918053766ef2bc250e2b2f82fe38d809fa75b6
Ice3man543 <nizamulrana@gmail.com>
2018-07-29 05:45:35 +0530

    Added a generic cli and fixed some stuff

--- altdns.go
+++ altdns.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"sync"
@@ -9,14 +10,9 @@ import (
 	"github.com/bobesa/go-domain-util/domainutil"
 )
 
-const (
-	Dictionary = "words.txt"
-)
-
 // AltDNS holds words, etc
 type AltDNS struct {
 	PermutationWords []string
-	OutputChan       chan<- string
 }
 
 func (a *AltDNS) insertDashes(domain string, results chan string) {
@@ -88,6 +84,20 @@ func (a *AltDNS) insertWordsSubdomains(domain string, results chan string) {
 	}
 }
 
+// New Returns a new altdns object
+func New(wordList string) *AltDNS {
+	altdns := AltDNS{}
+
+	f, _ := os.Open(wordList)
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		altdns.PermutationWords = append(altdns.PermutationWords, scanner.Text())
+	}
+
+	return &altdns
+}
+
 // Permute permutes a given domain and sends output on a channel
 func (a *AltDNS) Permute(domain string) chan string {
 	wg := sync.WaitGroup{}
@@ -131,27 +141,40 @@ func (a *AltDNS) Permute(domain string) chan string {
 }
 
 func main() {
+	var wordlist, host, list string
+	hostList := []string{}
+	flag.StringVar(&host, "host", "", "Host to generate permutations for")
+	flag.StringVar(&list, "l", "", "List of hosts to generate permutations for")
+	flag.StringVar(&wordlist, "w", "words.txt", "Wordlist to generate permutations with")
 
-	urls := []string{"aa.bb.cc.dd.com", "www.ee.com"}
+	flag.Parse()
 
-	altnds := &AltDNS{}
-
-	f, _ := os.Open(Dictionary)
-	scanner := bufio.NewScanner(f)
-
-	for scanner.Scan() {
-		altnds.PermutationWords = append(altnds.PermutationWords, scanner.Text())
+	if host == "" && list == "" {
+		fmt.Printf("%s: no host/hosts specified!\n", os.Args[0])
+		os.Exit(1)
 	}
 
+	if host != "" {
+		hostList = append(hostList, host)
+	} else if list != "" {
+		f, _ := os.Open(list)
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			hostList = append(hostList, scanner.Text())
+		}
+	}
+
+	altdns := New(wordlist)
 	jobs := sync.WaitGroup{}
 
-	for _, u := range urls {
+	for _, u := range hostList {
 		subdomain := domainutil.Subdomain(u)
 		domainSuffix := domainutil.Domain(u)
 		jobs.Add(1)
 		go func(domain string) {
 			defer jobs.Done()
-			for r := range altnds.Permute(subdomain) {
+			for r := range altdns.Permute(subdomain) {
 				fmt.Printf("%s.%s\n", r, domainSuffix)
 			}
 		}(u)
---
+++ altdns_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/bobesa/go-domain-util/domainutil"
+)
+
+func TestPermute(t *testing.T) {
+	urls := []string{"abc.xyz.freelancer.com", "aa.bb.cc"}
+
+	altdns := New("words.txt")
+
+	jobs := sync.WaitGroup{}
+
+	for _, u := range urls {
+		subdomain := domainutil.Subdomain(u)
+		domainSuffix := domainutil.Domain(u)
+		jobs.Add(1)
+		go func(domain string) {
+			defer jobs.Done()
+			for r := range altdns.Permute(subdomain) {
+				fmt.Printf("%s.%s\n", r, domainSuffix)
+			}
+		}(u)
+	}
+
+	jobs.Wait()
+}
